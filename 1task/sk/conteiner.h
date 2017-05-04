3dd9fc4abdf75e9b57b8c12ba7b754c1a6edfed9
medifh <medifh@bk.ru>
2017-05-05 01:19:05 +0700

    1task

---
+++ 1task/sk/conteiner.h
@@ -0,0 +1,129 @@
+#include < stdio.h >
+
+template < typename T >
+
+class Conteiner 
+{
+private:
+	struct Element
+	{
+		T m_value;
+		Element * m_next;
+		Element * m_previous;
+		int tag;
+	};
+	Element * m_head;
+	Element * m_tail;
+	int m_quanity;
+public:
+	Conteiner( )
+	{
+		m_head = NULL;
+		m_tail = NULL;
+		m_quanity = 0;
+	};
+	~Conteiner( )
+	{
+		Element * current = m_head;
+		Element * current2 = m_head;
+		while ( current != NULL )
+		{
+			current2 = (current -> m_previous);
+			delete current;
+			current = current2;
+		}
+	};
+	Conteiner( const Conteiner & orig )
+	{
+		this -> m_head = orig.m_head;
+		this -> m_tail = orig.m_tail;
+	}
+	 void DelTail() // удаление последнего
+	 {
+		 if (m_tail != NULL)
+		 {
+			 Element* current = m_tail->m_next;
+			 delete m_tail;
+			 m_tail = current;
+			 m_quanity --;
+		 }
+	 }
+	 void DelHead( ) // удаление первого
+	 {
+		 if (m_head != NULL)
+		 {
+			 Element * current = m_head -> m_previous;
+			 delete m_head;
+			 m_head = current;
+			 m_quanity --;
+		 }
+	 }
+	 T GetHead( ) // получить значение первого
+	 {
+		 if (m_head != NULL)
+		 {
+			 return m_head -> m_value;
+		 } 
+
+	 }
+	 T GetTail( ) // получить значение последнего
+	 {
+		 if (m_tail != NULL)
+		 {
+			 return m_tail -> m_value;
+		 } 
+	 }
+	 T GetElement(int numb) // получить элемент номера numb (перебор)
+	 {
+		 int i = 0;
+		 Element* current = m_tail;
+		 while (( current != NULL ) || ( i = numb ))
+		 {
+			 current = current -> m_next;
+			 i ++;
+		 } 
+		 return current;
+	 }
+	 void PutHead(T newval) // добавить первый элемент
+	 {
+		 Element * newhead = new Element;
+		 newhead -> m_next = NULL;
+		 newhead -> m_previous = m_head;
+		 newhead -> m_value = newval;
+		 m_head = newhead;
+		 if ( m_tail == NULL ) m_tail = m_head;
+		 m_quanity ++;
+	 }
+	 void PutTail( T newval ) // добавить последний элемент
+	 {
+		 Element * newtail = new Element;
+		 newtail -> m_next = m_tail;
+		 newtail -> m_previous = NULL;
+		 newtail -> m_value = newval;
+		 m_tail = newtail;
+		  if ( m_head == NULL ) m_head = m_tail;
+		 m_quanity ++;
+	 }
+	 bool IsEmpty( ) //проверить на пустоту
+	 {
+		 return ( m_head == NULL ); 
+	 }
+	 void Clearn( )
+	 {
+		Element * current = m_head;
+		Element * current2 = m_head;
+		while ( current != NULL )
+		{
+			current2 = ( current -> m_previous );
+			delete current;
+			current = current2;
+		}
+		m_head = NULL;
+		m_tail = NULL;
+		m_quanity = 0;
+	 }
+	 int GetQuanity( ) // получить количество элементов
+	 {
+		 return m_quanity;
+	 }
+};
