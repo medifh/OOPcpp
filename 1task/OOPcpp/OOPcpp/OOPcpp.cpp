c3fb7c7aa261652147008b56b49ff9d9a5a749ed
medifh <medifh@bk.ru>
2017-05-05 03:14:34 +0700

    OOPcpp

---
+++ 1task/OOPcpp/OOPcpp/OOPcpp.cpp
@@ -0,0 +1,20 @@
+#include "conteiner.h"
+#include < stdio.h >
+#include < stdlib.h >
+#include < iostream >
+#include < windows.h >
+void main()
+{
+	Conteiner < int > a;
+	a.PutHead( 3 );
+	a.PutHead( 4 );
+	a.PutHead( 5 );
+	a.PutTail( 2 );
+	std::cout << a.IsEmpty( ) << "\n" << a.GetTail( ) << "\n" << a.GetHead() << "\n" << a.GetQuanity();
+	a.DelHead( );
+	a.DelTail( );
+	std::cout << "\n----\n" << a.IsEmpty() << "\n" << a.GetTail( ) << "\n" << a.GetHead() << "\n"<< a.GetQuanity( );
+	a.Clearn( );
+	std::cout << "\n=" << a.IsEmpty( );
+	system( "pause" );
+}
